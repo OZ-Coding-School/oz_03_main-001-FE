8f797aa29fd7a42d839d7286e709b254c283d141
dos_no <tlatms777@gmail.com>
2024-07-17 19:21:36 +0900

    💡 Comment:페이지 실행을 위한 임시 주석처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './Header';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Join from './pages/Join';
-import Welcome from './pages/Welcome';
-import Order from './pages/Order';
-import OrderDetail from './pages/OrderDetail';
-import OrderHistories from './pages/OrderHistories';
-import OrderHistory from './pages/OrderHistory';
+// import Header from './Header';
+// import Home from './pages/Home';
+// import Login from './pages/Login';
+// import Join from './pages/Join';
+import Welcome from './pages/Welcome/Welcome';
+// import Order from './pages/Order';
+// import OrderDetail from './pages/OrderDetail';
+// import OrderHistories from './pages/OrderHistories';
+// import OrderHistory from './pages/OrderHistory';
 
 function App() {
   return (
     <Router>
-      <Header />
+      {/* <Header /> */}
 
       <Routes>
-        <Route path='/' element={<Home />} />
+        {/* <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/join' element={<Join />} />
+        <Route path='/join' element={<Join />} /> */}
         <Route path='/welcome' element={<Welcome />} />
-        <Route path='/order' element={<Order />} />
+        {/* <Route path='/order' element={<Order />} />
         <Route path='/orderdetail' element={<OrderDetail />} />
         <Route path='/orderhistories' element={<OrderHistories />} />
-        <Route path='/orderhistories/:id' element={<OrderHistory />} />
+        <Route path='/orderhistories/:id' element={<OrderHistory />} /> */}
       </Routes>
     </Router>
   );
