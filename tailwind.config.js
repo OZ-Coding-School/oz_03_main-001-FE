9e09f5d63e6e99c112f51adf36c950ca9a01e5f2
소빈 <sobin11@naver.com>
2024-07-19 16:58:12 +0900

    💄 Style: 움직이는 배경 이미지 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,42 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    extend: {
+      keyframes: {
+        bg1_move: {
+          '0%, 100%': {
+            transform: 'scale(1) rotate(0deg) translate(0px, 0px)',
+          },
+          '50%': {
+            transform: 'scale(1.3) rotate(5deg) translate(-3px, 3px)',
+          },
+        },
+        bg2_move: {
+          '0%, 100%': {
+            transform: 'scale(1) rotate(0deg) translate(0px, 0px)',
+          },
+          '50%': {
+            transform: 'scale(1.1) rotate(-10deg) translate(1px, 3px)',
+          },
+        },
+        bg3_move: {
+          '0%, 100%': {
+            transform: 'scale(1) rotate(0deg) translate(0px, 0px)',
+          },
+          '50%': {
+            transform: 'scale(1.1) rotate(-5deg) translate(1px, 3px);',
+          },
+        },
+      },
+      animation: {
+        bg1_move: 'bg1_move 5s ease-in-out infinite;', 
+        bg2_move: 'bg2_move 7s ease-in-out infinite;', 
+        bg3_move: 'bg3_move 9s ease-in-out infinite;', 
+      },
+      boxShadow: {
+        'btn': '2px 2px 10px 2px rgba(0, 0, 0, 0.15)',
+      }
+    },
     colors: {
       'transparent': 'transparent',
       'white': '#ffffff',
