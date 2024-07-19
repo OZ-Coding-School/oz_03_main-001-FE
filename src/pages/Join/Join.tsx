fe04594b0c48423e8808fbf2dd4604e9b8931294
Yi-HyeonJu <guswwn1925@gmail.com>
2024-07-19 11:25:45 +0900

    ✨ Feat: 오류 메세지를 표시하는 모달 추가

--- src/pages/Join/Join.tsx
+++ src/pages/Join/Join.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useJoinStore } from '../../store/useJoinStore';
 import logo from '../../assets/images/dosilockLogo.png';
+import Modal from './Modal/Modal';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Join = () => {
+  // 페이지 이동을 위한 네비게이트
+  const navigate = useNavigate();
+
   // useJoinStore를 통해 필요한 상태와 상태 업데이트 함수들을 가져옵니다.
   const {
     nameValue,
@@ -34,10 +40,11 @@ const Join = () => {
   // 이메일, 아이디, 패스워드 정규표현식
   const regexs = {
     emailRegex: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-    idRegex: /^[a-zA-Z][a-zA-Z0-9_-]{2,19}$/,
+    idRegex: /^[a-zA-Z][a-zA-Z0-9_-]{4,19}$/,
     pwRegex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?!.*\s).{8,}$/,
   };
 
+  // 이름 검사
   const regexsName = (value: string) => {
     if (!value) {
       setNameError('이름을 입력해 주세요');
@@ -48,9 +55,10 @@ const Join = () => {
     }
   };
 
+  // 이메일 검사
   const regexsEmail = (value: string) => {
     if (!regexs.emailRegex.test(value)) {
-      setEmailError('유효한 이메일 주소를 입력해 주세요');
+      setEmailError('올바른 이메일 주소를 입력해 주세요');
       return false;
     } else {
       setEmailError('');
@@ -58,9 +66,14 @@ const Join = () => {
     }
   };
 
+  // 아이디 검사
   const regexsId = (value: string) => {
-    if (!regexs.idRegex.test(value)) {
-      setIdError('유효한 아이디를 입력해 주세요');
+    if (!/^[a-zA-Z]/.test(value)) {
+      setIdError('아이디의 첫 글자는 알파벳으로 입력해 주세요');
+    } else if (!regexs.idRegex.test(value)) {
+      setIdError(
+        '알파벳, 숫자, 밑줄(_), 대시(-), 사용하여 5자 이상 13자 이하로 입력해 주세요'
+      );
       return false;
     } else {
       setIdError('');
@@ -68,9 +81,14 @@ const Join = () => {
     }
   };
 
+  // 비번 검사
   const regexsPassword = (value: string) => {
-    if (!regexs.pwRegex.test(value)) {
-      setPassError('유효한 비밀번호를 입력해 주세요');
+    if (value.length < 8) {
+      setPassError('최소 8자 이상 입력해 주세요');
+    } else if (!regexs.pwRegex.test(value)) {
+      setPassError(
+        '숫자, 소문자, 대문자, 특수문자를 각각 최소 하나 이상 포함하여 입력해 주세요'
+      );
       return false;
     } else {
       setPassError('');
@@ -78,6 +96,7 @@ const Join = () => {
     }
   };
 
+  // 비번 확인 검사
   const regexsRePassword = (value: string, rePass: string) => {
     if (value !== rePass) {
       setRePassError('비밀번호가 일치하지 않습니다');
@@ -88,6 +107,7 @@ const Join = () => {
     }
   };
 
+  // 각 인풋 벨류들 온체인지
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNameValue(e.target.value);
     regexsName(e.target.value);
@@ -113,8 +133,18 @@ const Join = () => {
     regexsRePassword(passValue, e.target.value);
   };
 
-  const postJoin = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  // 모달 상태 관리
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+
+  // 회원가입 하기 버튼 클릭시 실행되는 함수
+  const postJoin = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    // 새로고침 방지
     e.preventDefault();
+
+    // 유효성 검사를 모두 마치면
     if (
       regexsName(nameValue) &&
       regexsEmail(emailValue) &&
@@ -122,7 +152,61 @@ const Join = () => {
       regexsPassword(passValue) &&
       regexsRePassword(passValue, rePassValue)
     ) {
-      alert('회원가입 성공');
+      //서버에 포스트 시도
+      try {
+        const response = await axios.post('/api/v1/users/singup', {
+          username: nameValue,
+          email: emailValue,
+          id: idValue,
+          password: passValue,
+        });
+
+        // 성공하면 알레르기로 페이지 이동
+        if (response.data.success) {
+          // 응답 데이터 성공 시 알러지 페이지로 이동
+          navigate('/all');
+        } else {
+          // 중복된 이메일 또는 아이디에 대한 오류 처리
+          // 둘 다 문제
+          if (response.data === 'duplicate_email' && 'duplicate_username') {
+            setEmailError('이미 사용 중인 이메일입니다.');
+            setIdError('이미 사용 중인 아이디입니다.');
+
+            setModalOpen(true);
+            setModalMessage('아이디와 이메일을 확인해 주세요.');
+
+            setTimeout(() => {
+              setModalOpen(false);
+            }, 4000);
+
+            // 이메일만 문제
+          } else if (response.data === 'duplicate_email') {
+            setEmailError('이미 사용 중인 이메일입니다.');
+
+            setModalOpen(true);
+            setModalMessage('이메일을 확인해 주세요.');
+
+            setTimeout(() => {
+              setModalOpen(false);
+            }, 4000);
+            // 아이디 문제
+          } else if (response.data === 'duplicate_id') {
+            setIdError('이미 사용 중인 아이디입니다.');
+
+            setModalOpen(true);
+            setModalMessage('아이디를 확인해 주세요.');
+
+            setTimeout(() => {
+              setModalOpen(false);
+            }, 4000);
+          }
+        }
+        // 에러가 발생하면 에러 문구 모달창 띄우기
+      } catch (error) {
+        console.error('에러 메세지 : ', error);
+        setModalOpen(true);
+        setModalMessage('서버 문제로 잠시 후 다시 시도해 주세요.');
+      }
     }
   };
 
@@ -131,13 +215,14 @@ const Join = () => {
       <div>
         <img className='h-[73px] w-[200px]' src={logo} alt='로고' />
       </div>
-      <form className='h-[598px] w-[490px] rounded-[28px] border border-border px-[40px] pb-[46px] pt-[40px]'>
+      <form className='w-[490px] rounded-[28px] border border-border px-[40px] pb-[46px] pt-[40px]'>
         <span className='text-xl font-medium leading-10 text-main'>
           회원가입
         </span>
         <div className='flex flex-col gap-[14px] pb-[35px] pt-[21px]'>
           <div>
             <input
+              id='name-input'
               className={`h-[60px] w-[410px] rounded-xl border px-[20px] py-[12px] ${nameError ? 'border-primary' : 'border-border'}`}
               type='text'
               placeholder='이름'
@@ -152,6 +237,7 @@ const Join = () => {
           </div>
           <div>
             <input
+              id='email-input'
               className={`h-[60px] w-[410px] rounded-xl border px-[20px] py-[12px] ${emailError ? 'border-primary' : 'border-border'}`}
               type='email'
               placeholder='이메일'
@@ -166,6 +252,7 @@ const Join = () => {
           </div>
           <div>
             <input
+              id='id-input'
               className={`h-[60px] w-[410px] rounded-xl border px-[20px] py-[12px] ${idError ? 'border-primary' : 'border-border'}`}
               type='text'
               placeholder='아이디'
@@ -180,6 +267,7 @@ const Join = () => {
           </div>
           <div>
             <input
+              id='password-input'
               className={`h-[60px] w-[410px] rounded-xl border px-[20px] py-[12px] ${passError ? 'border-primary' : 'border-border'}`}
               type='password'
               placeholder='비밀번호'
@@ -194,6 +282,7 @@ const Join = () => {
           </div>
           <div>
             <input
+              id='re-password-input'
               className={`h-[60px] w-[410px] rounded-xl border px-[20px] py-[12px] ${rePassError ? 'border-primary' : 'border-border'}`}
               type='password'
               placeholder='비밀번호 확인'
@@ -207,17 +296,23 @@ const Join = () => {
             )}
           </div>
         </div>
-        <button
-          className={`h-[60px] w-[410px] rounded-xl border border-border px-[20px] py-[12px] text-lg font-bold text-white ${
-            isValueEmpty
-              ? 'bg-primary hover:bg-primary-hover'
-              : 'bg-gray20 hover:bg-gray30'
-          }`}
-          type='submit'
-          onClick={postJoin}
-        >
-          회원가입 하기
-        </button>
+        <div className='relative'>
+          <button
+            id='submit-button'
+            className={`h-[60px] w-[410px] rounded-xl border border-border px-[20px] py-[12px] text-lg font-bold text-white ${
+              isValueEmpty
+                ? 'bg-primary hover:bg-primary-hover'
+                : 'bg-gray20 hover:bg-gray30'
+            }`}
+            type='submit'
+            onClick={postJoin}
+          >
+            회원가입 하기
+          </button>
+          <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+            {modalMessage}
+          </Modal>
+        </div>
       </form>
     </div>
   );
