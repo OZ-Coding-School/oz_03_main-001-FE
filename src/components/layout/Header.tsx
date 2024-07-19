63bd8ebc035632f73b5b76695f889a5dd801e5f5
Yi-HyeonJu <guswwn1925@gmail.com>
2024-07-19 19:19:28 +0900

    ✨ Feat: Link 기능 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Logo from '../../assets/images/dosilockLogo.png';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/images/dosirockLogo.png';
 
 const Header = () => {
   return (
@@ -8,22 +8,22 @@ const Header = () => {
       <div className='inner mx-auto flex h-[75px] w-[1770px] items-center justify-between'>
         <div className='flex items-center'>
           <h1>
-            <a href='#' aria-label='도시락도 락이다'>
-              <img src={Logo} aria-hidden className='w-[118px]' />
-            </a>
+            <Link to={'/'} aria-label='도시락도 락이다'>
+              <img src={Logo} alt='' aria-hidden className='w-[118px]' />
+            </Link>
           </h1>
-          <a
-            href='#'
+          <Link
+            to={'/order'}
             className='ml-[58px] text-lg font-medium hover:text-primary'
           >
             주문하기
-          </a>
-          <a
-            href='#'
+          </Link>
+          <Link
+            to={'/order'}
             className='ml-[62px] text-lg font-medium hover:text-primary'
           >
             커뮤니티
-          </a>
+          </Link>
         </div>
         <button className='h-[38px] w-[85px] rounded-full bg-border font-medium'>
           로그인
