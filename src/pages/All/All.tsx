318d7f66b421f65f51d88979e0ac935034159018
Yi-HyeonJu <guswwn1925@gmail.com>
2024-07-19 17:51:29 +0900

    ✨ Feat: 알레르기 기능 구현 중

--- src/pages/All/All.tsx
+++ src/pages/All/All.tsx
@@ -30,29 +30,49 @@ const All = () => {
       <div>
         <img className='h-[73px] w-[200px]' src={logo} alt='로고' />
       </div>
-      <form className='w-[630px] rounded-[28px] border border-border px-[40px] pb-[46px] pt-[40px]'>
-        <p className='h-10 text-xl font-medium leading-10'>추가정보 입력</p>
-        <p className='text-sm font-normal'>
+      <form className='h-[560px] w-[630px] rounded-[28px] border border-border px-[40px] pb-[46px] pt-[40px]'>
+        <p className='h-10 text-xl font-medium leading-10 text-main'>
+          추가정보 입력
+        </p>
+        <p className='text-sm font-normal text-caption'>
           회원님의 알레르기 정보를 체크해주세요
         </p>
-        <div className='flex pt-[30px]'>
-          <input id='no_all' name='알레르기 없음' type='checkbox' />
-          <label htmlFor='no_all' className='text-base font-normal'>
+        <div className='pt-[30px]'>
+          <input
+            id='no_all'
+            name='알레르기 없음'
+            type='checkbox'
+            className='h-5 w-5'
+          />
+          <label htmlFor='no_all' className='px-5 text-base font-normal'>
             알레르기 없음
           </label>
         </div>
-        <div className='pt-[35px]'>
+        <div className='flex flex-wrap gap-[20px] pb-[27px] pt-[35px]'>
           {allList.map((all, index) => {
             return (
-              <div key={index}>
-                <input id={`yse_${all}`} name={all} type='checkbox' />
-                <label htmlFor={`yse_${all}`}>{all}</label>
+              <div
+                key={index}
+                className='flex w-[120px] items-center justify-start'
+              >
+                <input
+                  id={`yse_${all}`}
+                  name={all}
+                  type='checkbox'
+                  className='h-5 w-5'
+                />
+                <label
+                  htmlFor={`yse_${all}`}
+                  className='px-5 text-base font-normal text-main'
+                >
+                  {all}
+                </label>
               </div>
             );
           })}
         </div>
         <button
-          className='h-[60px] w-[410px] rounded-xl border border-border bg-primary px-[20px] py-[12px] text-lg font-bold text-white hover:bg-primary-hover'
+          className='h-[70px] w-[549px] rounded-xl border border-border bg-primary px-[20px] py-[12px] text-lg font-bold text-white hover:bg-primary-hover'
           type='submit'
         >
           입력 완료
