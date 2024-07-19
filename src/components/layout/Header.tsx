b8f5e2caa37ad06b1d2a4869dae9d8a7a4123de5
소빈 <sobin11@naver.com>
2024-07-19 16:58:46 +0900

    💄 Style: prettier 적용

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,19 +1,36 @@
-import React from 'react'
-import Logo from '../../assets/images/dosilockLogo.png'
+import React from 'react';
+import Logo from '../../assets/images/dosilockLogo.png';
 
 const Header = () => {
   return (
-    <header className='bg-white fixed top-0 left-0 right-0'>
-        <div className='inner h-[75px] w-[1770px] mx-auto flex items-center justify-between '>
-            <div className='flex items-center'>
-                <h1><a href='#' aria-label='도시락도 락이다'><img src={Logo} aria-hidden className='w-[118px]'/></a></h1>
-                <a href='#' className='ml-[58px] text-lg hover:text-primary font-medium'>주문하기</a>
-                <a href='#' className='ml-[62px] text-lg hover:text-primary font-medium'>커뮤니티</a>
-            </div>
-            <button className='bg-border w-[85px] h-[38px] rounded-full font-medium'>로그인</button>
+    <header className='fixed left-0 right-0 top-0 z-50 bg-white'>
+      {/* <div className='h-[100px] bg-secondary'></div> */}
+      <div className='inner mx-auto flex h-[75px] w-[1770px] items-center justify-between'>
+        <div className='flex items-center'>
+          <h1>
+            <a href='#' aria-label='도시락도 락이다'>
+              <img src={Logo} aria-hidden className='w-[118px]' />
+            </a>
+          </h1>
+          <a
+            href='#'
+            className='ml-[58px] text-lg font-medium hover:text-primary'
+          >
+            주문하기
+          </a>
+          <a
+            href='#'
+            className='ml-[62px] text-lg font-medium hover:text-primary'
+          >
+            커뮤니티
+          </a>
         </div>
+        <button className='h-[38px] w-[85px] rounded-full bg-border font-medium'>
+          로그인
+        </button>
+      </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
