a818b1f2ac755bab08cda1633f84fba64c9f8b92
소빈 <sobin11@naver.com>
2024-07-24 14:32:11 +0900

    💄 Style: 헤더 버튼 스타일 업데이트: 페이지에 따라 강조 색상 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,13 +5,15 @@ import Logo from '../../assets/images/dosirockLogo.png';
 
 const Header = () => {
   const location = useLocation();
+  const orderPaths: string[] = ['/order', '/orderDetail', '/orderHistory'];
+  const CommunityPaths: string[] = ['/community'];
 
   return (
     <>
       {location.pathname === '/' ? <div className='h-[42px] bg-white' /> : null}
 
       <header className='sticky left-0 right-0 top-0 z-50 bg-white'>
-        <div className='inner headerBreak:w-[1775px] mx-auto flex h-[75px] w-[95%] items-center justify-between'>
+        <div className='inner mx-auto flex h-[75px] w-[95%] items-center justify-between headerBreak:w-[1775px]'>
           <div className='flex items-center'>
             <h1>
               <Link to='/'>
@@ -24,13 +26,13 @@ const Header = () => {
             </h1>
             <Link
               to='/order'
-              className='ml-[58px] text-lg font-medium hover:text-primary'
+              className={`ml-[58px] text-lg font-medium hover:text-primary ${orderPaths.includes(location.pathname) ? '!font-bold text-primary' : ''}`}
             >
               주문하기
             </Link>
             <Link
-              to=''
-              className='ml-[62px] text-lg font-medium hover:text-primary'
+              to='/community'
+              className={`ml-[62px] text-lg font-medium hover:text-primary ${CommunityPaths.includes(location.pathname) ? '!font-bold text-primary' : ''}`}
             >
               커뮤니티
             </Link>
@@ -40,6 +42,7 @@ const Header = () => {
           </button>
         </div>
       </header>
+      <div></div>
     </>
   );
 };
