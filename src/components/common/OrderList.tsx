7a135de7b922869f338980a1e2c3a0deff57d5ad
Yi-HyeonJu <guswwn1925@gmail.com>
2024-07-30 10:55:26 +0900

    💄 Style: 오더리스트 너비 수정

--- src/components/common/OrderList.tsx
+++ src/components/common/OrderList.tsx
@@ -6,7 +6,7 @@ type OrderListProps = {
 
 const OrderList = ({ name, details, price }: OrderListProps) => {
   return (
-    <div className='flex flex-col justify-center rounded-lg border border-border px-[25px] py-[20px]'>
+    <div className='flex w-[100%] flex-col justify-center rounded-lg border border-border px-[25px] py-[20px]'>
       <p className='text-lg font-medium text-main'>{name}</p>
       <p className='pt-[10px] text-base font-normal text-caption'>{details}</p>
       <p className='text-right'>{price.toLocaleString()}원</p>
