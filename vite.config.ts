61c4d1acd431ce31746e69c2e560d5164273d0d5
2_현주 <164320612+Yi-HyeonJu@users.noreply.github.com>
2024-07-31 19:32:49 +0900

    🚑 Fix: 테일윈드 대문자 -> 소문자 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindCSS from 'tailwindCSS';
+import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: { postcss: { plugins: [tailwindCSS, autoprefixer] } },
+  css: { postcss: { plugins: [tailwindcss, autoprefixer] } },
 });
