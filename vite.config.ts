40dac17e8d09e2f05557fdf467ffbf164ea36c88
Yi-HyeonJu <guswwn1925@gmail.com>
2024-07-31 19:27:38 +0900

    🚑 Fix: 테일윈드 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindCSS from 'tailwindCSS';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), !tailwindCSS()],
+  plugins: [react()],
+  css: { postcss: { plugins: [tailwindCSS, autoprefixer] } },
 });
