09f2d079fae80df519aee5b16577db22c31a1744
Yi-HyeonJu <guswwn1925@gmail.com>
2024-08-01 17:25:43 +0900

    ✨ Feat: 주소 검색 기능, 도시락 수량 정보 받아오기

--- src/components/common/OrderList.tsx
+++ src/components/common/OrderList.tsx
@@ -2,13 +2,17 @@ type OrderListProps = {
   name: string;
   details: string;
   price: number;
+  quantity: number;
 };
 
-const OrderList = ({ name, details, price }: OrderListProps) => {
+const OrderList = ({ name, details, quantity, price }: OrderListProps) => {
   return (
     <div className='flex w-[100%] flex-col justify-center rounded-lg border border-border px-[25px] py-[20px]'>
-      <p className='text-lg font-medium text-main'>{name}</p>
-      <p className='pt-[10px] text-base font-normal text-caption'>{details}</p>
+      <div className='flex items-center justify-between'>
+        <p className='text-lg font-medium text-main'>{name}</p>
+        <p className='text-sm font-normal text-gray30'>수량 : {quantity}</p>
+      </div>
+      <p className='py-3 text-base font-normal text-caption'>{details}</p>
       <p className='text-right'>{price.toLocaleString()}원</p>
     </div>
   );
